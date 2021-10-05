0ce0e57a0e5a144f420c59b04f079bcd3b3b6334
day qinyanjui <49823575+qinyanjuidavid@users.noreply.github.com>
2021-10-05 12:43:17 +0300

    Search View

--- posting/api/views.py
+++ posting/api/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from posting.models import BlogPost
 from posting.api.serializers import BlogPostSerializer
-from rest_framework import generics
+from rest_framework import generics, mixins
+from django.db.models import Q
 
 
-class BlogPostAPIView(generics.CreateAPIView):
+class BlogPostAPIView(mixins.CreateModelMixin, generics.ListAPIView):
     serializer_class = BlogPostSerializer
 
     def get_queryset(self):
-        return BlogPost.objects.all()
+        qs = BlogPost.objects.all()
+        query = self.request.GET.get('q')
+        if query is not None:
+            qs = qs.filter(Q(title__icontains=query),
+                           Q(content__icontains=query))
+        return query
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
 class BlogPostRudView(generics.RetrieveUpdateDestroyAPIView):
@@ -28,3 +37,6 @@ class BlogPostRudView(generics.RetrieveUpdateDestroyAPIView):
 # RetriveAPIView
 # RetrieveUpdateDestoyAPIView
 # RetriveDestoyAPIView
+# ListAPIView
+# CreateAPIView
+#
