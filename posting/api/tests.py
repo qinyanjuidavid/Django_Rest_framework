345658882612ec3c460f0633da661fe2947fe0a7
day qinyanjui <49823575+qinyanjuidavid@users.noreply.github.com>
2021-10-06 10:12:12 +0300

    User login authentication

--- posting/api/tests.py
+++ posting/api/tests.py
@@ -4,11 +4,15 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework.reverse import reverse as api_reverse
 from rest_framework import status
+from rest_framework_jwt.settings import api_settings
+
+payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+encode_handler = api_settings.JWT_ENCODE_HANDLER
 
 
 class BlogPostAPITestCase(APITestCase):
     def setUp(self):
-        user_obj = User(username="testdayuser", email="test@test.com")
+        user_obj = User(username="testuser", email="test@test.com")
         user_obj.set_password("somerandompassword")
         user_obj.save()
         blog_post = BlogPost.objects.create(
@@ -59,3 +63,47 @@ class BlogPostAPITestCase(APITestCase):
 
         response = self.client.put(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_update_item_with_user(self):
+        blog_post = BlogPost.objects.first()
+        print(blog_post.content)
+        url = blog_post.get_api_url()
+        data = {
+            "title": "Some random title",
+            "content": "some more content"
+        }
+        user_obj = User.objects.first()
+        payload = payload_handler(user_obj)
+        token_rsp = encode_handler(payload)
+        self.client.credentials(HTTP_AUTHORIZATION="JWT"+token_rsp)
+
+        response = self.client.put(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        print(response.data)
+
+    def test_user_ownership(self):
+        owner = User.objects.create(username="testuser")
+        blog_post = BlogPost.objects.create(
+            user=owner,
+            title="New title",
+            content="Some random content")
+        user_obj = User.objects.first()
+        self.assertNotEqual(user_obj.username, owner.username)
+        payload = payload_handler(user_obj)
+        token_rsp = encode_handler(payload)
+        self.client.credentials(HTTP_AUTHORIZATION="JWT"+token_rsp)
+        url = blog_post.get_api_url()
+        data = {'title': "Some random title",
+                "content": "Some more content"}
+        response = self.client.put(url, data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_user_login(self):
+        data = {
+            "username": "testuser",
+            "password": "somerandompassword"
+        }
+        url = api_reverse("api-login")
+        response = self.client.post(url, data)
+        print("DATA::", response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- src/urls.py
+++ src/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/postings/', include('posting.api.urls',
-                                  namespace="api-postings"),)
+                                  namespace="api-postings"),),
+
+    path('api-token-auth/', obtain_jwt_token, name="api-login")
 ]
